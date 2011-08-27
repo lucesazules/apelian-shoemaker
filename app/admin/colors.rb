5b962a28f11a026ddb2d341c17bf652698d01dca
Dwayne Macgowan <dwaynemac@gmail.com>
2011-08-27 10:40:01 -0300

    set color from backend

--- app/admin/colors.rb
+++ app/admin/colors.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Color do
-  
+
 end
--- app/admin/shoes.rb
+++ app/admin/shoes.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Shoe do
-  
+  form do |f|
+    f.inputs do
+      f.input :article_number
+      f.input :name
+    end
+    f.inputs do
+      f.input :colors, :as => :check_boxes
+    end
+    f.buttons
+  end
 end
