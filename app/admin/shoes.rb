273a3f78f3c241dc2415d0b1336ee90186c6fca8
Dwayne Macgowan <dwaynemac@gmail.com>
2011-08-27 12:13:08 -0300

    tune shoes backend show

--- app/admin/shoes.rb
+++ app/admin/shoes.rb
@@ -1,4 +1,22 @@
 ActiveAdmin.register Shoe do
+
+  show :title => :name do
+    panel I18n.t('admin.shoes.show.colors') do
+      ul do
+        shoe.colors.each do |color|
+          li color.name
+        end
+      end
+    end
+    panel I18n.t('admin.shoes.show.sizes') do
+      ul do
+        shoe.sizes.each do |size|
+          li size.number
+        end
+      end
+    end
+  end
+
   form do |f|
     f.inputs do
       f.input :article_number
