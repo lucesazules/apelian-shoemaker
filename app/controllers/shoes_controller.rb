4cd06fbb306cf1427dc2776875d051eded6cebf1
Dwayne Macgowan <dwaynemac@gmail.com>
2011-08-31 21:21:20 -0300

    minimal db interaction

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,11 +1,13 @@
 class ShoesController < ApplicationController
   def index
+    @shoes = Shoe.all
   end
 
   def show
+    @shoe = Shoe.find(params[:id])
   end
 
   def tendencies
+    @shoes = Shoe.all
   end
-
 end
