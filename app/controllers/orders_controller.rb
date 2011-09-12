882c03a53f745d6e9c932a422c8860df7924ac6c
Dwayne Macgowan <dwaynemac@gmail.com>
2011-09-12 00:47:16 -0300

    print order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,23 @@
 class OrdersController < ApplicationController
 
+  layout 'order'
+
+  def show
+    @order = Order.find params[:id]
+  end
+
   def new
     @order = Order.new
     5.times{ @order.order_details.build }
-    render :layout => 'order'
   end
 
   def create
     @order = Order.new(params[:order])
     if @order.save!
       OrdersMailer.order_form(@order).deliver
-      redirect_to new_order_path, :flash => { :success => "Orden enviada" }
+      redirect_to @order
     else
-      render :action => :new, :layout => 'order'
+      render :action => :new
     end
   end
 end
