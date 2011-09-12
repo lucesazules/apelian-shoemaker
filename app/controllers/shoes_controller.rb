b8441a20dba813d9ff66a2510f12b25c89affc88
Dwayne Macgowan <dwaynemac@gmail.com>
2011-09-12 02:23:37 -0300

    3rd attempt

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,5 +1,7 @@
 class ShoesController < ApplicationController
 
+  before_filter :set_scope, :only => :show
+
   def index
     @search = Shoe.search(params[:search])
     @shoes = @search.order(:name).page(params[:page]).per(18)
@@ -7,21 +9,24 @@ class ShoesController < ApplicationController
 
   def show
     @shoe = Shoe.find(params[:id], :include => [:colors, :sizes])
-    scope = nil
-    if params[:scope].to_s == 'trend'
-      scope = Shoe.trend
-    else
-      @search = Shoe.search(:heel_equals => @shoe.heel)
-      scope = @search
-    end
-    @previous_shoe = @shoe.previous_shoe(:article_number, scope)
-    @next_shoe = @shoe.next_shoe(:article_number, scope)
-    @total_shoes = scope.count
-    @index = scope.where("article_number < ?", @shoe.article_number).count+1
+    @previous_shoe = @shoe.previous_shoe(:article_number, @scope)
+    @next_shoe = @shoe.next_shoe(:article_number, @scope)
+    @total_shoes = @scope.count
+    @index = @scope.where("article_number < ?", @shoe.article_number).count+1
   end
 
   def trend
     @shoes = Shoe.trend.page(params[:page]).per(18)
+  end
+
+  private
+  def set_scope
+    if params[:scope].to_s == 'trend'
+      @scope = Shoe.trend
+    else
+      @search = Shoe.search(:heel_equals => @shoe.heel)
+      @scope = @search
+    end
   end
 
 end
