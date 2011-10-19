4c59ae60ed23d6da4c093a19d3578b23f746872f
Dwayne Macgowan <dwaynemac@gmail.com>
2011-10-18 21:45:56 -0300

    deliver mails to Google Apps account

--- app/mailers/orders_mailer.rb
+++ app/mailers/orders_mailer.rb
@@ -1,6 +1,6 @@
 class OrdersMailer < ActionMailer::Base
   default from: "no-reply@apelian.com.ar"
-  default to: "apelianjuan@hotmail.com"
+  default to: "info@apelian.com.ar"
 
   # Email of order form to seller
   def order_form(order)
